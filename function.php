fee7df98c802fb41dad9305a94dd0e2cf1a5131a
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-13 08:39:06 +0300

    added

--- function.php
+++ function.php
@@ -6,6 +6,6 @@ class system
     {
         $this->username = $user;
         $this->password = $pass;
-        echo "Username is <b> $user </b> and Password is <b>$pass</b>";
+      echo "Username is <b> $user </b> and Password is $pass";
     }
 }
